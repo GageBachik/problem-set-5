06b66b5c4b181804cb25a25387ba36f6d21065be
gbachik <gbachik@gmail.com>
2014-07-29 12:11:28 -0600

    finished all the non bonuses

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-var express = require('express');
+var alphabetSoup = function(str){
+	var letters = str.toLowerCase().split('');
+	console.log(letters.sort().join(''));
+};
 
-var app = express();
+// alphabetSoup('hello');
 
-app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
-});
+var vowelCount = function(str){
+	var letters = str.toLowerCase().split('');
+	var count = 0;
+	for (var i = 0; i < letters.length; i++) {
+		// console.log(letters[i].charCodeAt());
+		if (letters[i].charCodeAt() === 97 || letters[i].charCodeAt() === 101 || letters[i].charCodeAt() === 105|| letters[i].charCodeAt() === 111 || letters[i].charCodeAt() === 117) {
+			count++
+		}
+	}
+	console.log(count);
+};
 
-var server = app.listen(5962, function() {
-	console.log('Express server listening on port ' + server.address().port);
-});
+// vowelCount('All cows eat grass');
+
+var coinDeterminer = function(){
+
+};
